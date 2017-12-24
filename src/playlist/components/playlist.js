0b904510270a9f888022dfc5c283d685ad3e7f22
Ivan Robles <irae45@gmail.com>
2017-12-24 14:37:26 -0600

    Agregamos componentes funcionales para los iconos del reproductor

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import Media from './media.js';
 import PropTypes from 'prop-types';
 import './playlist.css';
+import Play from '../../icons/components/play';
+import Pause from '../../icons/components/pause';
+import Volume from '../../icons/components/volume';
+import FullScreen from '../../icons/components/full-screen';
 
 class Playlist extends Component {
   render() {
@@ -21,6 +25,22 @@ class Playlist extends Component {
                 <h3>{item.title}</h3>
                 <p>{item.description}</p>
                 <div className="Playlist">
+                  <Play
+                    size={50}
+                    color="gray"
+                  />
+                  <Pause
+                    size={50}
+                    color="gray"
+                  />
+                  <Volume
+                    size={50}
+                    color="gray"
+                  />
+                  <FullScreen
+                    size={50}
+                    color="gray"
+                  />
                   {
                     playlist.map((item) => {
                       return <Media {...item} key={item.id}/>;
