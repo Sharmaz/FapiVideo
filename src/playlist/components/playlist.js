53a7a3d5937b50dc67436df74429b2a0bb3d9568
Ivan Robles <irae45@gmail.com>
2017-12-24 13:43:26 -0600

    Ahora nuestro Playlist component despliega varias categorías con sus Media component

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -5,13 +5,30 @@ import './playlist.css';
 
 class Playlist extends Component {
   render() {
-    const playlist = this.props.data.categories[0].playlist;
+    let counter = 0;
+    const category = this.props.data.categories
+    // const playlist = this.props.data.categories[counter].playlist;
+
     console.log(this.props.data);
     return (
-      <div className="Playlist">
+      <div className="Category">
         {
-          playlist.map((item) => {
-            return <Media {...item} key={item.id}/>;
+          category.map((item) => {
+            console.log(item);
+            const playlist = item.playlist;
+            return (
+              <div>
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+                <div className="Playlist">
+                  {
+                    playlist.map((item) => {
+                      return <Media {...item} key={item.id}/>;
+                    })
+                  } 
+                </div>
+              </div>
+            )
           })
         } 
       </div>
