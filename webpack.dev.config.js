4a28dc37613cd58a9c3a7a59c470c625502c3ac8
Ivan Robles <irae45@gmail.com>
2017-12-24 18:23:53 -0600

    Cambiamos la estructura del proyecto para utilizar components ui y cointainers

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    "fapi-video": path.resolve(__dirname, 'index.js'),
+    "home": path.resolve(__dirname, 'src/entries/home.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
