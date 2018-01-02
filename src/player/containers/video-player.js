f56b653e1a3c4797501059497fb1877315dcba45
Ivan Robles <irae45@gmail.com>
2018-01-01 21:45:17 -0600

    Creamos un componente para los controles de play y pause

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from './video';
 import Title from '../components/title';
+import PlayPause from '../components/play-pause';
 
 class VideoPlayer extends Component {
+  state = {
+    pause: true,
+  }
+  togglePlay = (event) => {
+    this.setState({
+      pause: !this.state.pause,
+    })
+  }
   render() {
     return (
       <VideoPlayerLayout>
@@ -13,6 +22,10 @@ class VideoPlayer extends Component {
         <Video
           autoplay={true}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+        />
+        <PlayPause
+          pause={this.state.pause}
+          handleClick={this.togglePlay}
         />
       </VideoPlayerLayout>
     );
